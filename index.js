590505f1feea843781dbb9e8732a5c41636ca5fd
Logan Miner <loganrobertminer@gmail.com>
2019-09-10 19:42:03 -0400

    init more files and folders

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('hello world')
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 1000;
+
+// Middleware
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+// routes
+require('./routes/api-routes')(app);
+require('./routes/html-routes')(app);
+
+// listen event 
+const server = app.listen(PORT, () => {
+    console.log('listening on port' + PORT + "\nhttp://localhost:" + PORT);
+})
